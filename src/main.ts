18cca56f311dcf270dbe92afc7065a2f0d4bd7a3
Antony Harfield <antonyharfield@gmail.com>
2020-01-19 23:03:57 +0700

    Add basic styles

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = false
 
-Vue.use(Buefy)
+Vue.use(Buefy, { defaultIconPack: 'fas' })
 
 new Vue({
   router,
--- src/pages/Site.vue
+++ src/pages/Site.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="site">
-    <b-navbar>
+    <b-navbar fixed-top="true" type="is-success" wrapper-class="container">
       <template slot="brand">
         <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          <img
-            src="https://i.pinimg.com/736x/79/b0/23/79b023e1fd83ed2fedbbbfd88ca6f7e1.jpg"
-            alt="NU x LINE"
-          /> NU x LINE Mentorship Programme
+          <strong>NU x LINE Mentorship Programme</strong>
         </b-navbar-item>
       </template>
       <template slot="end">
         <b-navbar-item tag="router-link" :to="{ path: '/' }">About</b-navbar-item>
         <b-navbar-item tag="router-link" :to="{ path: '/schedule' }">Schedule</b-navbar-item>
         <b-navbar-item tag="router-link" :to="{ path: '/apply' }">How to apply</b-navbar-item>
+        <b-navbar-item tag="span">
+          <a class="button is-success is-inverted">
+            <b-icon pack="fas" icon="rocket"></b-icon>
+            <strong>Apply Now</strong>
+          </a>
+        </b-navbar-item>
       </template>
     </b-navbar>
     <router-view />
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>NU x LINE Mentorship Programme</strong> is supported by
+        </p>
+        <p>
+          <img src="@/assets/supported-by-logos.png" style="width: 425px" />
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
