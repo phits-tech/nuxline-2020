a085ef13c42db470b3bb85505b90bc77252820ff
Antony Harfield <antonyharfield@gmail.com>
2020-01-23 11:57:45 +0700

    Make the router scroll to the top when switching pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -42,7 +42,10 @@ const routes: RouteConfig[] = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
 
 export default router
