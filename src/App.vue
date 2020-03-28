c55fd84f789680fc9d18d802d286d8628ca2b905
AjahnCharles <31621095+AjahnCharles@users.noreply.github.com>
2020-03-28 10:30:46 +0700

    Template - Show the menu on tablet / half-screen desktop - Fix spacing around location

--- src/App.vue
+++ src/App.vue
@@ -11,48 +11,24 @@ $primary: #2adb32;
 $primary-invert: findColorInvert($primary);
 
 $colors: (
-  "white": (
-    $white,
-    $black
-  ),
-  "black": (
-    $black,
-    $white
-  ),
-  "light": (
-    $light,
-    $light-invert
-  ),
-  "dark": (
-    $dark,
-    $dark-invert
-  ),
-  "primary": (
-    $primary,
-    $primary-invert
-  ),
-  "info": (
-    $info,
-    $info-invert
-  ),
-  "success": (
-    $success,
-    $success-invert
-  ),
-  "warning": (
-    $warning,
-    $warning-invert
-  ),
-  "danger": (
-    $danger,
-    $danger-invert
-  )
+  "white"   : ($white, $black),
+  "black"   : ($black, $white),
+  "light"   : ($light, $light-invert),
+  "dark"    : ($dark, $dark-invert),
+  "primary" : ($primary, $primary-invert),
+  "info"    : ($info, $info-invert),
+  "success" : ($success, $success-invert),
+  "warning" : ($warning, $warning-invert),
+  "danger"  : ($danger, $danger-invert)
 );
 
 // Links
 $link: $primary;
 $link-invert: $primary-invert;
 $link-focus-border: $primary;
+
+// Nav
+$navbar-breakpoint: $tablet;
 
 @import "~bulma";
 @import "~buefy/src/scss/buefy";
